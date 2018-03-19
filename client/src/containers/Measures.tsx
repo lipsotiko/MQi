844f39fbce2f836a6399ce4c517a36f67f522f36
lipsotiko <lipsotiko@gmail.com>
2018-03-18 21:22:53 -0400

    learning how to use react and typescript

--- client/src/containers/Measures.tsx
+++ client/src/containers/Measures.tsx
@@ -1,14 +1,39 @@
 import * as React from 'react';
 import { Component } from 'react';
 
-class Measures extends Component {
+interface State {
+    measures: JSON;
+}
+
+interface Props {
+}
+
+export default class Measures extends Component<Props, State> {
+
+    public state: State;
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            measures: JSON
+        };
+    }
+
+    componentDidMount() {
+        return fetch('http://localhost:8080/measure')
+            .then(response => response.json())
+            .then(measures => this.setState({measures}));
+    }
+
     render() {
         return (
             <div>
                 <h2>Measures</h2>
+                <p>{JSON.stringify(this.state.measures)}</p>
+                {/*{this.state.measures.map((m, i) =>*/}
+                    {/*<p key={i}>{i}. {m}</p>*/}
+                {/*)}*/}
             </div>
         );
     }
 }
-
-export default Measures;
