84c3df084c4e7cb91da08d150b4ec7aab289a00c
lipsotiko <lipsotiko@gmail.com>
2018-06-23 12:56:44 -0400

    java streams and good test coverage

--- server/src/test/java/io/egia/mqi/RuleParamUtilityTest.java
+++ server/src/test/java/io/egia/mqi/RuleParamUtilityTest.java
@@ -8,10 +8,17 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest(classes = {StandaloneConfig.class})
@@ -31,11 +38,26 @@ public class RuleParamUtilityTest {
     @Captor
     private ArgumentCaptor<RuleParam> captor = ArgumentCaptor.forClass(RuleParam.class);
 
+    private String packagePrefix = "io.egia.mqi.rules";
+
+    private Reflections reflections = new Reflections(packagePrefix
+            , new SubTypesScanner(false)
+            , new TypeAnnotationsScanner());
+
     @Test
     public void rulesInsertTheirMetaDataInMqiDb() throws ClassNotFoundException {
         RuleParamUtility subject = new RuleParamUtility(ruleParamRepository);
         subject.saveRuleParams();
         verify(ruleParamRepository, times(expected.length)).save(captor.capture());
         assertThat(captor.getAllValues()).contains(expected);
+    }
+
+    @Test
+    public void allClassesInTheRulesPackageAreAreAnnotated() {
+        Set<String> allClasses = reflections.getAllTypes();
+        String[] expected = allClasses.stream().map(String::toString).toArray(String[]::new);
+        Set<String> allAnnotatedClasses = reflections.getTypesAnnotatedWith(Rule.class).stream()
+                .map(Class::getName).collect(Collectors.toSet());
+        assertThat(allAnnotatedClasses).contains(expected);
     }
 }
