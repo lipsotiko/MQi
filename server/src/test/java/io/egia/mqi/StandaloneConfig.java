353a1ad67c2ec6932c111ad70b5b68f2b864a963
lipsotiko <lipsotiko@gmail.com>
2018-06-23 22:11:33 -0400

    cleaner logs, no more hibernate errors

--- server/src/test/java/io/egia/mqi/StandaloneConfig.java
+++ server/src/test/java/io/egia/mqi/StandaloneConfig.java
@@ -30,7 +30,7 @@ public class StandaloneConfig {
 
 	private Properties additionalProperties() {
 		Properties properties = new Properties();
-		properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
 		properties.setProperty("hibernate.show_sql","false");
 		properties.setProperty("hibernate.format_sql","true");
