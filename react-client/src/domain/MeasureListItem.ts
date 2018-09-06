769b4cc206cc88379c94386959c65fdea4f5d8d9
lipsotiko <lipsotiko@gmail.com>
2018-09-05 23:35:38 -0400

    removed react client code

--- react-client/src/domain/MeasureListItem.ts
+++
@@ -1,9 +0,0 @@
-export class MeasureListItem {
-    measureId: number;
-    measureName: string;
-
-    static fromJSON(json: JSON): MeasureListItem {
-        let measure = Object.create(MeasureListItem.prototype);
-        return Object.assign(measure, json);
-    }
-}
